44d8365d425d9535c34c06fa4f88d4599db30418
DaniloGaray <141675715+DaniloGaray@users.noreply.github.com>
2023-08-07 17:08:42 -0300

    commit By Danilo Garay

---
+++ JavaApplication31/src/javaapplication31/Danilo.java
@@ -0,0 +1,11 @@
+
+package javaapplication31;
+
+public class Danilo {
+    
+        private final String nombre = "Danilo Garay";
+    
+    public String Saludar(){
+        return "Hola Me llamo "+ nombre;}
+    
+}
