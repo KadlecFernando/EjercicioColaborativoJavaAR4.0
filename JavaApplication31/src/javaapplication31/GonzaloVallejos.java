b82a4f8c906cdedffc7e178b6b0c481254d87294
FixedFun <54387710+FixedFun@users.noreply.github.com>
2023-08-07 17:05:24 -0300

    Mi nombre
    Hola hola

---
+++ JavaApplication31/src/javaapplication31/GonzaloVallejos.java
@@ -0,0 +1,10 @@
+package javaapplication31;
+
+public class GonzaloVallejos {
+    private final String nombre = "Gonzalo";
+    
+        public String Saludar(){
+        return "Hola Me llamo "+ nombre;
+    }
+
+}
