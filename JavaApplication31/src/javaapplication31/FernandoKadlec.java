0ad9d3b77c3263841a7293b8903153c0c4ab5ed8
KadlecFernando <kadlec.fernando13@gmail.com>
2023-08-07 17:03:44 -0300

    SegundoCommit

--- JavaApplication31/src/javaapplication31/FernandoKadlec.java
+++ JavaApplication31/src/javaapplication31/FernandoKadlec.java
@@ -6,7 +6,7 @@ public class FernandoKadlec {
     private final String nombre = "Fernando Kadlec";
     
     public String Saludar(){
-        return "Hola Me llamo "+ nombre;
+        return "Hola Me llamo  "+ nombre;
     }
         
        
