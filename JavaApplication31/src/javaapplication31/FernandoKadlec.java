f87edcab16a9a25282703f8f3284e3b5b3f5efaf
KadlecFernando <kadlec.fernando13@gmail.com>
2023-08-07 16:43:33 -0300

    PrimerCommitByFernandoKadlec

---
+++ JavaApplication31/src/javaapplication31/FernandoKadlec.java
@@ -0,0 +1,16 @@
+
+package javaapplication31;
+
+
+public class FernandoKadlec {
+    private final String nombre = "Fernando Kadlec";
+    
+    public String Saludar(){
+        return "Hola Me llamo "+ nombre;
+    }
+        
+       
+    
+  
+    
+}
---
+++ JavaApplication31/src/javaapplication31/JavaApplication31.java
@@ -0,0 +1,15 @@
+
+package javaapplication31;
+
+
+public class JavaApplication31 {
+
+   
+    public static void main(String[] args) {
+        FernandoKadlec fernando = new FernandoKadlec();
+        System.out.println(fernando.Saludar());
+        
+        
+    }
+    
+}
